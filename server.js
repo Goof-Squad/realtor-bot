52495db7c83f467dcfdd59b4d6ce786b1b075aae
a-arshad <ali.arshad@move.com>
2018-10-04 13:44:35 -0700

    heroku stuff

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
-// First we need to import the HTTP module. This module contains all the logic for dealing with HTTP requests.
-var http = require('http');
-
-// We define the port we want to listen to. Logically this has to be the same port than we specified on ngrok.
-const PORT=4390;
-
-// We create a function which handles any requests and sends a simple response
-function handleRequest(request, response){
-    response.end('Ngrok is working! -  Path Hit: ' + request.url);
-}
-
-// We create the web server object calling the createServer function. Passing our request function onto createServer guarantees the function is called once for every HTTP request that's made against the server
-var server = http.createServer(handleRequest);
-
-// Finally we start the server
-server.listen(PORT, function(){
-    // Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
+var express = require(‘express’);
+var port = process.env.PORT || 3000;
+var app = express();
+app.get(‘/’, function (req, res) {
+ res.send(JSON.stringify({ Hello: ‘World’}));
+});
+app.listen(port, function () {
+ console.log(`Example app listening on port !`);
 });
