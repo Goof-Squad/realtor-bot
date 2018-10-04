98f45d324bcfb4f9a2b50a5ad6174066fe979b22
vmagay <vlad.magay@move.com>
2018-10-04 16:51:23 -0700

    Python parser and server update

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 const express = require("express");
-
+var request = require('request');
+const https = require('https');
+const http = require('http');
 const app = express();
-const port = process.env.PORT || 3000;
+var extract = require('pdf-text-extract')
+const fs = require('fs');
+const exec = require('child_process').exec;  
+var pdf_table_extractor = require("pdf-table-extractor");
 
-app.listen(port, () => {
-    console.log(`Go to http://localhost:${port}`);
-});
+var server = app.listen(3000, function () {
+    var host = server.address().address
+    var port = server.address().port
+    
+    console.log("Example app listening at http://%s:%s", host, port)
+})
+
+app.get('/', function (req, res) {
+    pdf_table_extractor("menu.pdf",success,error);
+    //PDF parsed
+    function success(result)
+    {
+        fs.writeFileSync("menu.json", JSON.stringify(result));
+    }
+    //Error
+    function error(err)
+    {
+        console.error('Error: ' + err);
+    }
+})
