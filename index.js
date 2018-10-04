2337e8e7e5df8aad362a88d165b47f5e43ed77b5
a-arshad <ali.arshad@move.com>
2018-10-04 15:12:41 -0700

    slack bots api added

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
-var RtmClient = require('@slack/client').RtmClient;
-var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
-var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+var SlackBot = require("slackbots");
+    var channel = "general";
 
-var rtm = new RtmClient('xoxb-450841990886-449315733300-wTAbCRNDa7A3mTjZcXBitRva');
-rtm.start();
+    var bot = new SlackBot({
+        token: "xoxb-450841990886-449315733300-wTAbCRNDa7A3mTjZcXBitRva",
+        name: "Slacky"
+    });
 
-let channel;
-let bot;
-
-rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
-  for (const c of rtmStartData.channels) {
-      if (c.is_member && c.name ==='jamiestestchannel') { channel = c.id }
-  }
-  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}`);
-   
-  bot = '<@' + rtmStartData.self.id + '>';
-});
-
-rtm.on(RTM_EVENTS.MESSAGE, function(message) {
-    if (message.channel === channel)
-        rtm.sendMessage("oof", message.channel);
-});
+    bot.on("start", function() {
+        bot.postMessageToChannel(channel, "Hello world!");
+        console.log("Hello world!");
+    });
